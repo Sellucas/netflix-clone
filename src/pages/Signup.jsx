303f2714f2d54a147962806bcd315684ccce4972
Lucas Sell Machado <75432770+Sellucas@users.noreply.github.com>
2022-12-01 13:28:06 -0300

    signin page

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -10,7 +10,7 @@ const Signup = () => {
                 <div className='fixed w-full px-4 py-24 z-50'>
                     <div className='max-w-[450px] h-[600px] mx-auto bg-black/75 text-white'>
                         <div className='max-w-[320px] mx-auto py-16'>
-                            <h1 className='text-3xl font-bold'>Sign In</h1>
+                            <h1 className='text-3xl font-bold'>Sign Up</h1>
                             <form className='w-full flex flex-col py-4'>
                                 <input className='p-3 my-2 bg-gray-700 rounded' type="email" placeholder='Email' autoComplete='email' />
                                 <input className='p-3 my-2 bg-gray-700 rounded' type="password" placeholder='Password' autoComplete='current-password' />
